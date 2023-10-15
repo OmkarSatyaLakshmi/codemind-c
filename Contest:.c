6e8b2fff6c64457d96d52ee001069bf512f75941
OmkarSatyaLakshmi <147476229+OmkarSatyaLakshmi@users.noreply.github.com>
2023-10-15 11:04:05 +0530

    #include<stdio.h> int main( ) {     int X,A,B;     scanf("%d%d%d",&X ,&A, &B);      int ts = A+(2*B);     if ( ts>=X)     {         printf("Qualify");     }     else     {         printf("Not Qualify");     } }

---
+++ Contest:.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main( )
+{
+    int X,A,B;
+    scanf("%d%d%d",&X ,&A, &B);
+     int ts = A+(2*B);
+    if ( ts>=X)
+    {
+        printf("Qualify");
+    }
+    else
+    {
+        printf("Not Qualify");
+    }
+}
+
+
+
+
+
