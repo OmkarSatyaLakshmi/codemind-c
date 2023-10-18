2b5da0993d56a459e0c55c557760fa0dcf721f7a
OmkarSatyaLakshmi <147476229+OmkarSatyaLakshmi@users.noreply.github.com>
2023-10-18 10:45:00 +0530

    #include<stdio.h> #include<math.h> int main() { 	float x; 	scanf("%f",&x); 	int minc=ceil(x/4); 	printf("%d",minc); }

--- Cars.c
+++ Cars.c
@@ -1,14 +1,9 @@
-#include <stdio.h>
+#include<stdio.h>
+#include<math.h>
 int main()
 {
-	int x;
-	scanf("%d",&x);
-	if(x%4==0)
-	{
-		printf("%d",x/4);
-	}
-	else 
-	{
-		printf("%d",(x/4)+1);
-	}
+	float x;
+	scanf("%f",&x);
+	int minc=ceil(x/4);
+	printf("%d",minc);
 }
